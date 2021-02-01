fe19a4c2c71c4d5627248e667b8762c6e20b6077
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-02-01 12:52:20 +1100

    Product screen listing created

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Container } from 'react-bootstrap';
+import Home from './screens/HomeScreen';
+
 function App() {
   return (
     <>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>home</h1>
+          <Home />
         </Container>
       </main>
 
