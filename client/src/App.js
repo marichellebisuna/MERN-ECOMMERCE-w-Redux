41e230af055b5bb1c62e724442877fd42b7dc853
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-02-01 12:09:15 +1100

    Header and Footer created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 function App() {
   return (
     <>
-      <h1>home</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>home</h1>
+        </Container>
+      </main>
+
+      <Footer />
     </>
   );
 }
