efc020354d58c07ea57985cd7f05fc98b69e6278
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-02-02 10:30:37 +1100

    Serving products from backend

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+require('dotenv').config();
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.send('api is running.');
+});
+
+app.get('/api/products', (req, res, next) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res, next) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server connected to port ${PORT}`);
+});
