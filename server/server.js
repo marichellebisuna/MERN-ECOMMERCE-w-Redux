c022960cc9bceb4c775b662bb05c4bdbeb2b468b
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-02-02 12:55:59 +1100

    Connecting to the MongoDb

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import products from './data/products.js';
-dotenv.config();
+import MongoDb from './config/db.js';
+
+MongoDb();
 
 const app = express();
 
