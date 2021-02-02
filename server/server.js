793dcf6f75dc1cb3979ea505dd727ca5d5bdcc8c
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-02-02 11:54:09 +1100

    Implementing ES Module in Nodejs

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-require('dotenv').config();
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import products from './data/products.js';
+dotenv.config();
 
 const app = express();
 
