8e96258004816f249de1b48eb688f5f200cf68df
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-02-20 10:36:55 +1100

    Update user created

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -42,6 +42,31 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   Update user profile
+// @route  PUT api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found.');
+  }
+});
+
 // @desc   Register a new user
 // @route  POST api/users
 // @access Public
@@ -74,4 +99,4 @@ const registerUser = asyncHandler(async (req, res) => {
   res.send('success');
 });
 
-export { authUser, getUserProfile, registerUser };
+export { authUser, getUserProfile, registerUser, updateUserProfile };
