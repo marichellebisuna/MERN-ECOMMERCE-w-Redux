9228f5e1608240335c9657e972d481879ce36e2a
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-02-28 16:09:10 +1100

    Get Order by ID Endpoint

--- server/controller/orderController.js
+++ server/controller/orderController.js
@@ -1,4 +1,5 @@
 import Order from '../models/orderModel.js';
+import User from '../models/userModel.js';
 import asyncHandler from 'express-async-handler';
 
 // @desc   Create new order
@@ -35,4 +36,21 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+// @desc   Get order by ID
+// @route  GET /api/orders/:id
+// @access Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
+export { addOrderItems, getOrderById };
