eebb97f82fb2c3a7657f856bbf6a611fe5ba8c67
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-03-01 13:23:01 +1100

    Show Orders on Profile created

--- client/src/screens/OrderScreen.js
+++ client/src/screens/OrderScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
+import { Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
