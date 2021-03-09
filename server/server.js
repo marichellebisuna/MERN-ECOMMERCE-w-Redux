6c1a853c447fca929be29411be651dedbed7b547
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-03-09 12:09:35 +1100

    Create Review done

--- server/server.js
+++ server/server.js
@@ -13,9 +13,8 @@ MongoDb();
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
